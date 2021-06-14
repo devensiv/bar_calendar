abbdf9e8b2dceb4bc240987cbd5804256aa88eb0
devensiv <devensiv@gmail.com>
2021-06-14 20:54:50 +0200

    loop over all events to find the most recent one
    fixes #4

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ mod event;
 pub fn next_calendar_event(configfile: PathBuf) -> Result<Event, Box<dyn Error>> {
     let now = Utc::now();
     let reader = BufReader::new(File::open(configfile)?);
+    let mut closest = (Duration::max_value(), None);
     for line in reader.lines() {
         let line = line?;
 
@@ -26,25 +27,29 @@ pub fn next_calendar_event(configfile: PathBuf) -> Result<Event, Box<dyn Error>>
 
         // format output
         let duration = event.date.signed_duration_since(now);
-        if duration > Duration::seconds(0) {
-            let state = match duration.num_minutes() {
-                0..=5 => "Critical".to_string(),
-                6..=15 => "Warning".to_string(),
-                16..=60 => "Good".to_string(),
-                61..=120 => "Info".to_string(),
-                _ => "Idle".to_string(),
-            };
-            return Ok(Event {
-                text: event.note,
-                time_until: duration,
-                state: state,
-            });
+        if (duration > Duration::seconds(0)) & (duration < closest.0) {
+            closest = (duration, Some(event));
         }
+    }
+
+    if let Some(event) = closest.1 {
+        let state = match closest.0.num_seconds() {
+            0..=300 => "Critical".to_string(),
+            301..=900 => "Warning".to_string(),
+            901..=3600 => "Good".to_string(),
+            3601..=7200 => "Info".to_string(),
+            _ => "Idle".to_string(),
+        };
+        return Ok(Event {
+            text: event.note,
+            time_until: closest.0,
+            state: state,
+        });
     }
 
     Ok(Event {
         text: "No Events".to_string(),
-        state: "Good".to_string(),
+        state: "Info".to_string(),
         time_until: Duration::seconds(0),
     })
 }
