5f79016f265cf204d5de9f39b9ee18559f39efae
devensiv <devensiv@gmail.com>
2021-06-26 20:20:52 +0200

    closes #1

--- src/entry.rs
+++ src/entry.rs
@@ -1,14 +1,31 @@
 use chrono::{DateTime, Datelike, Duration, Utc};
 use std::error::Error;
 
-#[derive(Debug)]
+#[derive(Clone)]
 pub struct Entry {
     pub note: String,
     pub description: String,
     pub date: DateTime<Utc>,
+    mod_stack: Vec<fn(&DateTime<Utc>, &DateTime<Utc>) -> DateTime<Utc>>,
 }
 
 impl Entry {
+    /// Tries to create a version of this Entry with a date laying in the future of the given now value using the available wildcards
+    /// returns true if modifying the internal state to future worked
+    pub fn resolve_wildcards(&mut self, now: DateTime<Utc>, duration: &mut Duration) -> bool {
+        while self.mod_stack.len() > 0 {
+            let time_mod = self.mod_stack.pop().unwrap();
+            let new_date = time_mod(&self.date, &now);
+
+            *duration = new_date.signed_duration_since(now);
+            if duration > &mut Duration::seconds(0) {
+                self.date = new_date;
+                return true;
+            }
+        }
+        false
+    }
+
     pub fn from_string(data: &str, now: &DateTime<Utc>) -> Result<Entry, Box<dyn Error>> {
         type DateMod = fn(&DateTime<Utc>, &DateTime<Utc>) -> DateTime<Utc>;
         let mut parts = data.split_whitespace();
@@ -148,21 +165,27 @@ impl Entry {
             } else {
                 Err("Error parsing the minutes")?
             };
-        
             date_string.push_str(&format!("{}:{}", hours, minutes));
         }
 
         let date = if weekformat {
-            DateTime::parse_from_str(&date_string, "%z %Y %a %W %R")
+            DateTime::parse_from_str(&date_string, "%z %Y %W %a %R")
         } else {
             DateTime::parse_from_str(&date_string, "%z %Y %b %d %R")
-        }?
+        }
+        .map_err(|_| {
+            format!(
+                "Error parsing {} in {}",
+                date_string,
+                if weekformat { "weekmode" } else { "monthmode" }
+            )
+        })?
         .with_timezone(&Utc);
-
         Ok(Entry {
             note: parts.next().unwrap_or("No Note").to_string(),
             description: parts.next().unwrap_or("No description").to_string(),
             date,
+            mod_stack: wildcard_offset,
         })
     }
 }
